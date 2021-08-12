db6e9e63c8fa5d0f0ebf9ee22cdba6bc399f3706
Jabah Gundy <gundy157@outlook.com>
2021-08-12 03:43:20 -0400

    updated HomePage CSS

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,7 +1,33 @@
+import classes from "../components/Home.module.css";
 import React from "react";
+import Buildings from "./video/Buildings.mp4";
 
 function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <video
+        className={classes.videoTag}
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          hight: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-1",
+        }}
+      >
+        <source src={Buildings} type="video/mp4" />
+      </video>
+      {/* <div>
+        <h2 className={classes.statement}>Statement</h2>
+      </div> */}
+    </div>
+  );
 }
 
 export default HomePage;
